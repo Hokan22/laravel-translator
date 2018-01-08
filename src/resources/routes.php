f6f25b0eb39e14bdb6fd717189e4e1459625c712
Alexander Viertel <alexander-viertel@web.de>
2018-01-08 10:19:54 +0100

    fixed path to Controller

--- src/resources/routes.php
+++ src/resources/routes.php
@@ -4,15 +4,15 @@ if (! Hokan22\LaravelTranslator\TranslatorFacade::getConfigValue('custom_routes'
 
     Route::group(['prefix' => 'translator'], function () {
 
-        Route::get('/test', 'Hokan22\LaravelTranslator\resources\views\TranslatorAdminController@test')->name('translator.test');
+        Route::get('/test', 'Hokan22\LaravelTranslator\Controllers\TranslatorAdminController@test')->name('translator.test');
 
         Route::group(['prefix' => 'admin'], function () {
 
-            Route::get('/', 'Hokan22\LaravelTranslator\resources\views\TranslatorAdminController@index')->name('translator.admin');
-            Route::post('/', 'Hokan22\LaravelTranslator\resources\views\TranslatorAdminController@postIdentifier')->name('translator.post.admin');
+            Route::get('/', 'Hokan22\LaravelTranslator\Controllers\TranslatorAdminController@index')->name('translator.admin');
+            Route::post('/', 'Hokan22\LaravelTranslator\Controllers\TranslatorAdminController@postIdentifier')->name('translator.post.admin');
 
-            Route::get('/{id}', 'Hokan22\LaravelTranslator\resources\views\TranslatorAdminController@edit')->where('id', '[0-9]+')->name('translator.admin.edit');
-            Route::post('/{id}', 'Hokan22\LaravelTranslator\resources\views\TranslatorAdminController@postEdit')->where('id', '[0-9]+')->name('translator.admin.post.edit');
+            Route::get('/{id}', 'Hokan22\LaravelTranslator\Controllers\TranslatorAdminController@edit')->where('id', '[0-9]+')->name('translator.admin.edit');
+            Route::post('/{id}', 'Hokan22\LaravelTranslator\Controllers\TranslatorAdminController@postEdit')->where('id', '[0-9]+')->name('translator.admin.post.edit');
 
         });
     });
