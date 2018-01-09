06874c3e7d4b9f72809f162ef57cc92535fcdd01
Alexander Viertel <alexander-viertel@web.de>
2018-01-09 17:04:40 +0100

    simplified test view

--- src/resources/views/test.blade.php
+++ src/resources/views/test.blade.php
@@ -1,20 +1,15 @@
+<div>
+    <p>@translate('test2', ['test_parameter' => 'Test'])</p>
+    <p>@translate('test')</p>
 
-@extends('layout.main')
+    <p>@translate('test')</p>
+    <p>@translate('greeting', ['name' => 'John'])</p>
+    <p>@translate('multi_parameter_test',   [
+                                                'name' => 'John',
+                                                'surname' => 'Doe',
+                                                'test' => '<i>hallo</i>'
+                                            ])</p>
+    <p>@translate('test')</p>
+    <p>@translate('dump_translator')</p>
+</div>
 
-
-@section('content')
-    <div>
-        <p>@translate('test2', ['test_parameter' => 'Test'])</p>
-        <p>@translate('test')</p>
-
-        <p>@translate('test')</p>
-        <p>@translate('greeting', ['name' => 'John'])</p>
-        <p>@translate('multi_parameter_test',   [
-                                                    'name' => 'John',
-                                                    'surname' => 'Doe',
-                                                    'test' => '<i>hallo</i>'
-                                                ])</p>
-        <p>@translate('test')</p>
-        <p>@translate('dump_translator')</p>
-    </div>
-@endsection
