0731feeb874ec7d9807d3aac359d3ee2c01cf784
Alexander Viertel <alexander-viertel@web.de>
2018-01-16 10:33:17 +0100

    phpdoc test

--- src/Commands/CacheTranslationCommand.php
+++ src/Commands/CacheTranslationCommand.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
+ * PHP version 5.6
+ *
  * Artisan Command to cache Translations from the Database.
  *
  * @category ArtisanCommand
- * @package Hokan22\LaravelTranslator\commands
- * @author Alexander Viertel <alexander@aviertel.de>
- * @license http://opensource.org/licenses/MIT MIT
- * @link https://github.com/Hokan22/laravel-translator
+ * @package  Hokan22\LaravelTranslator\commands
+ * @author   Alexander Viertel <alexander@aviertel.de>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/Hokan22/laravel-translator
  */
 namespace Hokan22\LaravelTranslator\Commands;
 
@@ -21,10 +23,10 @@ use Illuminate\Support\Facades\DB;
  * Class CacheTranslationCommand
  *
  * @category ArtisanCommand
- * @package Hokan22\LaravelTranslator\commands
- * @author Alexander Viertel <alexander@aviertel.de>
- * @license http://opensource.org/licenses/MIT MIT
- * @link https://github.com/Hokan22/laravel-translator
+ * @package  Hokan22\LaravelTranslator\commands
+ * @author   Alexander Viertel <alexander@aviertel.de>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/Hokan22/laravel-translator
  */
 class CacheTranslationCommand extends Command
 {
